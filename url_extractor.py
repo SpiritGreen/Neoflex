aa61cbdd34ac5a97ef455ad0068f0f90eea5d5c0
SpiritGreen <79371068+SpiritGreen@users.noreply.github.com>
2023-05-15 21:41:49 +0300

    Create url_extractor.py
    Task #1

---
+++ url_extractor.py
@@ -0,0 +1,47 @@
+"""
+Извлеките доменное имя из URL-адреса
+
+Необходимо написать функцию, которая при задании URL адреса в виде строки анализирует
+только доменное имя и возвращает его в виде строки.
+
+Пример вывода функции:
+
+url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+
+url = "http://www.zombie-bites.com" -> domain name = "zombie-bites"
+
+url = "https://www.cnet.com" -> domain name = cnet
+"""
+
+
+def extract_url(url):
+    # removing unnecessary spaces at the beginning and end in url:
+    url = url.strip()
+
+    # clearing the beginning of url:
+    if url.startswith('http://'):
+        result = url[7:]
+    elif url.startswith('https://'):
+        result = url[8:]
+    else:
+        result = url
+    if result.startswith('www.'):
+        result = result[4:]
+
+    # clearing the end of the url:
+    dot_id = result.find('.')
+    result = result[:dot_id]
+    return result
+
+
+url1 = 'http://github.com/carbonfive/raygun'
+url2 = 'http://www.zombie-bites.com'
+url3 = 'https://www.cnet.com'
+url4 = 'github.com/carbonfive/raygun'
+url5 = 'www.cnet.com'
+
+print(extract_url(url1))
+print(extract_url(url2))
+print(extract_url(url3))
+print(extract_url(url4))
+print(extract_url(url5))
